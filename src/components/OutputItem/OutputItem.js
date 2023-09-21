1c24f3bc1a52787f81dc41ea1c10dc89ce8289ef
ghostb0x <ghostb0x.github@gmail.com>
2023-09-22 00:35:50 +0800

    Update OutputItem to use a select element to setLanguage state variable with selection

--- src/components/OutputItem/OutputItem.js
+++ src/components/OutputItem/OutputItem.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React from 'react';
 import styled from 'styled-components';
 import { Cross2Icon } from '@radix-ui/react-icons';
 
 function OutputItem(key) {
-
   const [hidden, setHidden] = React.useState(false);
 
-  const [language, setLanguage] = React.useState(null);
+  const [language, setLanguage] = React.useState("");
 
-  const [content, setContent] = React.useState(null)
+  const [content, setContent] = React.useState("");
 
-  const item = 
-  <OutputWrapper key={key}>
-    <CloseButton onClick={()=> setHidden(!hidden)}>
-      <CloseIcon />
-    </CloseButton>
-    <OutputLanguage>{language === null ? "Select Language" : language}</OutputLanguage>
-    <OutputText>{content === null ? "Select Language and input message" : content}</OutputText>
-  </OutputWrapper>
+  const languageSelect = (
+    <OutputLanguage
+      value={language}
+      onChange={(event) => {
+        setLanguage(event.target.value);
+      }}
+    >
+      <option value={""}>--Select Language--</option>
+      <option value="sp">Spanish</option>
+      <option value="pt">Portuguese</option>
+      <option value="de">German</option>
+      <option value="fr">French</option>
+    </OutputLanguage>
+  );
+
+  const item = (
+    <OutputWrapper key={key}>
+      <CloseButton onClick={() => setHidden(!hidden)}>
+        <CloseIcon />
+      </CloseButton>
+      {languageSelect}
+      <OutputText>
+        {content === null
+          ? 'Select Language and input message'
+          : content}
+      </OutputText>
+    </OutputWrapper>
+  );
 
   return hidden === false ? item : null;
 }
@@ -39,11 +58,9 @@ const CloseButton = styled.button`
   height: 40px;
   width: 40px;
 `;
-const CloseIcon = styled(Cross2Icon)`
-  
-`;
+const CloseIcon = styled(Cross2Icon)``;
 
-const OutputLanguage = styled.h2``;
+const OutputLanguage = styled.select``;
 
 const OutputText = styled.p`
   margin-top: 10px;
