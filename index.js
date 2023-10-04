15bcc6919680864093591ce895c894627658615d
ghostb0x <ghostb0x.github@gmail.com>
2023-10-05 00:34:31 +0800

    Create index.js for express backend to route API requests and securely store API secret key

--- index.js
+++ index.js
@@ -6,9 +6,30 @@ require('dotenv').config();
 
 const app = express();
 
+app.use(cors())
 
-app.get('/', (req,res) => {
-    res.json('hi girl')
+app.get('/translation', (req,res) => {
+    const options = {
+        method: 'POST',
+        url: 'https://google-translator9.p.rapidapi.com/v2',
+        headers: {
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+        'X-RapidAPI-Host': 'google-translator9.p.rapidapi.com',
+        },
+        data: {
+        q: req.query.q,
+        source: req.query.source,
+        target: req.query.target,
+        format: 'text',
+        },
+    };
+
+    axios.request(options).then((response) => {
+        res.json(response.data)
+    }).catch((error) => {
+           console.error(error);
+    })
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- src/components/AppProvider/AppProvider.js
+++ src/components/AppProvider/AppProvider.js
@@ -6,19 +6,31 @@ export const AppContext = React.createContext();
 function AppProvider({ children }) {
   const [queryText, setQueryText] = React.useState('');
 
+  // saved searches functionality:
+
+  // (v1) on click of "Add to Saved Searches" button
+  //    - bundle current queryText and current outputs into single js object
+  //    - push to "saved searches" object
+  //    - push updated "saved searches" obj storage
+  // (v1) on click of saved item in "Saved Searches" panel
+  //    - retrieve from local storage and populate queryInput and outputs with values
+  // (v1) on click of X button of saved item from "Saved Searches" panel, 
+  //    - remove item from local storage
+  //    - remove item from saved searches
+  // (v2) Upon first using "add to saved search"
+  //    - display warning alert that items saved in storage are not secure from hackers
+  //    - so no sensitive password data should ever be inputted and saved
+  //    - add acknowledgement before allowing
+
+
   const [fetchTranslate, setFetchTranslate] = React.useState(0);
 
   async function getTranslation(input_lang, output_lang) {
+    
     const options = {
-      method: 'POST',
-      url: 'https://google-translator9.p.rapidapi.com/v2',
-      headers: {
-        'content-type': 'application/json',
-        'X-RapidAPI-Key':
-          `${RapidAPIKey}`,
-        'X-RapidAPI-Host': 'google-translator9.p.rapidapi.com',
-      },
-      data: {
+      method: 'GET',
+      url: 'http://localhost:8000/translation',
+      params: {
         q: queryText,
         source: input_lang,
         target: output_lang,
